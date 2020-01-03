1adf728e854c4eb58dfbff04c325338464aef06c
Rogelio <jayarr12.rc@gmail.com>
2020-01-03 09:05:04 +0800

    Updates

---
+++ precache-manifest.ac634067a78d0f46da8005c93579e0ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4aad72ba8e339e42e90b768c07d919",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "4977dbf3e70966ab61d6",
+    "url": "/react-pages/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ea70b0c9f35a2e692048",
+    "url": "/react-pages/static/css/main.70fad6f4.chunk.css"
+  },
+  {
+    "revision": "4977dbf3e70966ab61d6",
+    "url": "/react-pages/static/js/2.54437d67.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-pages/static/js/2.54437d67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea70b0c9f35a2e692048",
+    "url": "/react-pages/static/js/main.75e10db2.chunk.js"
+  },
+  {
+    "revision": "159bd5c5910f1449ad7c",
+    "url": "/react-pages/static/js/runtime-main.23d63216.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
