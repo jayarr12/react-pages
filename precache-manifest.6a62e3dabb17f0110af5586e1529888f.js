ec9649af9350c9b473cec6b4e89c32dfa3fd659c
Rogelio <jayarr12.rc@gmail.com>
2020-01-22 14:52:14 +0800

    Updates

---
+++ precache-manifest.6a62e3dabb17f0110af5586e1529888f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5144e114052b6f892693d3fe22773ee7",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "9e6fe79677018e528f69",
+    "url": "/react-pages/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ce71de6482b9dc953ab3",
+    "url": "/react-pages/static/css/main.b0ea3685.chunk.css"
+  },
+  {
+    "revision": "9e6fe79677018e528f69",
+    "url": "/react-pages/static/js/2.9a357759.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-pages/static/js/2.9a357759.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce71de6482b9dc953ab3",
+    "url": "/react-pages/static/js/main.cd1f83d1.chunk.js"
+  },
+  {
+    "revision": "159bd5c5910f1449ad7c",
+    "url": "/react-pages/static/js/runtime-main.23d63216.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pages/precache-manifest.c7fb24044c47a303a43f2f48b8c8daf0.js"
+  "/react-pages/precache-manifest.6a62e3dabb17f0110af5586e1529888f.js"
 );
 
 self.addEventListener('message', (event) => {
